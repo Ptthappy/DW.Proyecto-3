24854fb83d41b891d5604bf1edc7966684b690ac
Luis Petrella <aax_00ap@hotmail.com>
2018-12-03 18:12:44 -0400

    1% de avance xd
    Y 100% de 100%n't

--- Main.js
+++ Main.js
@@ -3,10 +3,52 @@
 class pageEditor extends HTMLElement {
 	constructor() {
 		super();
+
+		let shadow = this.attachShadow({mode : 'open'});
+		shadow.innerHTML =
+			`
+			<style>
+				
+				#container {
+					background-color: black;
+					border : 5px solid black;
+					
+					width: 100%;
+					height: 100%;
+				}
+				
+				.btn-container {
+				  border: 2px dashed #e67e22;
+				  padding: 1.5em;
+				  text-align: center;
+				}
+				
+				.btn {
+				  background-color: #e67e22;
+				  border: 0;
+				  border-radius: 5px;
+				  color: white;
+				  padding: 1.5em;
+				  text-transform: uppercase;
+				}
+			  </style>
+			  
+			  	<div id="container">
+			  		
+				</div>
+			  
+			</style>
+			`;
+		let txt1 = document.createElement("div");
+		let txt2 = document.createElement("div");
+		let txt3 = document.createElement("div");
+
+
+
 	}
 }
 
-customElements.define('pg-editor', pageEditor);
+window.customElements.define('pg-editor', pageEditor);
 
 let x = new pageEditor();
 
