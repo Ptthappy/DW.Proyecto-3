164cad80fce03994885df743109c4d3bce8d77eb
Luis Petrella <aax_00ap@hotmail.com>
2018-12-03 23:42:52 -0400

    ui no
    Ok está horrible

--- Main.js
+++ Main.js
@@ -15,8 +15,8 @@ class pageEditor extends HTMLElement {
 					border : 2px solid darkslateblue;
 					border-radius: 3px;
 					
-					width: 550px;
-					height: 320px;
+					width: 340px;
+					height: 420px;
 					top: 50px;
 					left: 10px;
 				}
@@ -30,18 +30,74 @@ class pageEditor extends HTMLElement {
 				
 				.btn {
 					background-color: rgba(0, 0, 0, 0.5);
-				  	border-radius: 2px;
+				  	border-radius: 1px;
 				  	color: white;
+				  	border: none;
+				  	font-family: "Berlin Sans FB";
 				  	
 				  	position: absolute;
 				  	width: 95px;
 					height: 25px;
-					top: 60px;
-					left: 300px;
+					top: 180px;
+					left: 150px;
 				}
 				
 				#btn1 {
-									
+					
+				}
+				
+				#mt {
+					background-color: rgba(0, 0, 0, 0.3);
+				  	border-radius: 1px;
+				  	border: none;
+				  	
+				  	position: absolute;
+				  	width: 320px;
+					height: 190px;
+					top: 220px;
+					left: 10px;
+				}
+				
+				#txt1 {
+					position: absolute;
+					top: 40px;
+					left: 100px;
+				}
+				
+				#txt2 {
+					position: absolute;
+					top: 80px;
+					left: 100px;
+				}
+				
+				#txt3 {
+					position: absolute;
+					top: 120px;
+					left: 100px;
+				}
+				
+				#tf1 {
+					position: absolute;
+					top: 50px;
+					left: 10px;
+					font-family: "Berlin Sans FB";
+					font-size: larger;
+				}
+				
+				#tf2 {
+					position: absolute;
+					top: 90px;
+					left: 10px;
+					font-family: "Berlin Sans FB";
+					font-size: larger;
+				}
+				
+				#tf3 {
+					position: absolute;
+					top: 130px;
+					left: 10px;
+					font-family: "Berlin Sans FB";
+					font-size: larger;
 				}
 				
 			  </style>
@@ -52,6 +108,13 @@ class pageEditor extends HTMLElement {
 					</div>
 					
 					<button id="btn1" class="btn">Submit</button>
+					<textarea id="txt1" cols="30" rows="2"></textarea>
+					<textarea id="txt2" cols="30" rows="2"></textarea>
+					<textarea id="txt3" cols="30" rows="2"></textarea>
+					<div id="tf1">Nombre:</div>
+					<div id="tf2">Apellido:</div>
+					<div id="tf3">Cédula:</div>
+					<table id="mt"></table>
 				</div>
 			  
 			</style>
