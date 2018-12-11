db0bee8077e1a925f546acd7afe13fa55fae5a8c
Luis Petrella <aax_00ap@hotmail.com>
2018-12-11 08:32:55 -0400

    Ok aún no funciona pero está mejor
    Falta terminar de debuggear los listeners

--- Main.js
+++ Main.js
@@ -5,27 +5,6 @@ class pageEditor extends HTMLElement {
 		super();
 		this.shadow = this.attachShadow({mode: 'open'});
 
-		this.x1 = null;
-		this.x2 = null;
-		this.x3 = null;
-		this.x4 = null;
-		this.x5 = null;
-		this.x6 = null;
-		this.x7 = null;
-		this.x8 = null;
-		this.x9 = null;
-
-		let isBarPress, antX, antY, overEdge, up, dw, lf, rg, isMax = false, isHidden = false,
-			top, left, width, height, lastHeight;
-
-	}
-
-	connectedCallback() {
-		this.initializeWindow();
-		this.addObj([]);
-	}
-
-	initializeWindow() {
 		let container = document.createElement('div');
 		container.id = 'container';
 		container.style.position = 'absolute';
@@ -86,14 +65,14 @@ class pageEditor extends HTMLElement {
 		maxbtn.textContent = '💯';
 
 		//Divs invisibles para los listeners
-		let top = document.createElement('div');
-		top.id = 'top';
-		top.style.position = 'absolute';
-		top.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-		top.style.width = '99%';
-		top.style.height = '10px';
-		top.style.top = '-6px';
-		top.style.left = '0.5%';
+		let Top = document.createElement('div');
+		Top.id = 'top';
+		Top.style.position = 'absolute';
+		Top.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+		Top.style.width = '99%';
+		Top.style.height = '10px';
+		Top.style.top = '-6px';
+		Top.style.left = '0.5%';
 
 		let down = document.createElement('div');
 		down.id = 'down';
@@ -104,14 +83,14 @@ class pageEditor extends HTMLElement {
 		down.style.top = '100%';
 		down.style.left = '0.5%';
 
-		let left = document.createElement('div');
-		left.id = 'left';
-		left.style.position = 'absolute';
-		left.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-		left.style.width = '10px';
-		left.style.height = '105%';
-		left.style.top = '-2%';
-		left.style.left = '-6px';
+		let Left = document.createElement('div');
+		Left.id = 'left';
+		Left.style.position = 'absolute';
+		Left.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+		Left.style.width = '10px';
+		Left.style.height = '105%';
+		Left.style.top = '-2%';
+		Left.style.left = '-6px';
 
 		let right = document.createElement('div');
 		right.id = 'right';
@@ -127,10 +106,318 @@ class pageEditor extends HTMLElement {
 		topbar.appendChild(redbtn);
 		topbar.appendChild(minbtn);
 		topbar.appendChild(maxbtn);
-		container.appendChild(top);
+		container.appendChild(Top);
 		container.appendChild(down);
-		container.appendChild(left);
+		container.appendChild(Left);
 		container.appendChild(right);
+
+		this.x1 = this.shadow.getElementById('container');
+		this.x2 = this.shadow.getElementById('top-bar');
+		this.x3 = this.shadow.getElementById('top');
+		this.x4 = this.shadow.getElementById('down');
+		this.x5 = this.shadow.getElementById('left');
+		this.x6 = this.shadow.getElementById('right');
+		this.x7 = this.shadow.getElementById('redbtn');
+		this.x8 = this.shadow.getElementById('maxbtn');
+		this.x9 = this.shadow.getElementById('minbtn');
+
+		let isBarPress, antX, antY, overEdge, up, dw, lf, rg, isMax = false, isHidden = false, top, left, width, height, lastHeight;
+
+		this.x7.onmouseover = evt => {
+			this.x7.style.backgroundColor = 'rgb(255, 0, 0)';
+		};
+
+		this.x7.onmouseleave = evt => {
+			this.x7.style.backgroundColor = 'rgb(190, 0, 0)';
+		};
+
+		this.x7.onclick = evt => {
+			this.x1.remove();
+		};
+
+		this.x8.onmouseover = evt => {
+			this.x8.style.backgroundColor = 'rgb(70, 70, 100)';
+		};
+
+		this.x8.onmouseleave = evt => {
+			this.x8.style.backgroundColor = 'rgb(100, 100, 140)';
+		};
+
+		this.x8.onclick = evt => {
+			if(!isMax) {
+				top = this.x1.style.top;
+				left = this.x1.style.left;
+				width = this.x1.style.width;
+				height = this.x1.style.height;
+				this.x1.style.top = 0 + 'px';
+				this.x1.style.left = 0 + 'px';
+				this.x1.style.width = 100 + '%';
+				this.x1.style.height = 100 + '%';
+				isMax = true;
+			}
+
+			else {
+				this.x1.style.top = top;
+				this.x1.style.left = left;
+				this.x1.style.width = width;
+				this.x1.style.height = height;
+				isMax = false;
+			}
+		};
+
+		this.x9.onclick = () => {
+			if (!isHidden){
+				for (let elements of x1.children) {
+					console.log(elements.id === 'top-bar');
+					if (elements.id === 'top-bar')
+						continue;
+					elements.style.visibility = 'hidden';
+				}
+				lastHeight = document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('height');
+				this.x1.style.height = 28 + 'px';
+				isHidden = true;
+			}
+
+			else{
+				for (let elements of this.x1.children) {
+					console.log(elements.id === 'top-bar');
+					if (elements.id === 'top-bar')
+						continue;
+					elements.style.visibility = 'visible';
+				}
+
+				this.x1.style.height = lastHeight;
+				isHidden = false;
+			}
+
+		};
+
+		this.x9.onmouseover = evt => {
+			this.x9.style.backgroundColor = 'rgb(150, 150, 150)';
+		};
+
+		this.x9.onmouseleave = evt => {
+			this.x9.style.backgroundColor = 'rgb(200, 200, 200)';
+		};
+
+		this.x3.onmousemove = evt => {
+			this.x3.style.cursor = 'n-resize';
+		};
+
+		this.x3.onmousedown = evt => {
+			overEdge = true;
+			up = true;
+			antX = evt.clientX;
+			antY = evt.clientY;
+		};
+
+		this.x4.onmousemove = evt => {
+			this.x4.style.cursor = 's-resize';
+		};
+
+		this.x4.onmousedown = evt => {
+			overEdge = true;
+			dw = true;
+			antX = evt.clientX;
+			antY = evt.clientY;
+		};
+
+		this.x5.onmousemove = evt => {
+			let i = parseInt(document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('top'));
+			let j = parseInt(document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('height'));
+
+			if (evt.clientY < i + 10 && evt.clientY > i - 10)
+				this.x5.style.cursor = 'nw-resize';
+
+			else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
+				this.x5.style.cursor = 'sw-resize';
+
+			else
+				this.x5.style.cursor = 'w-resize';
+		};
+
+		this.x5.onmousedown = evt => {
+			overEdge = true;
+
+			let i = parseInt(document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('top'));
+			let j = parseInt(document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('height'));
+
+			if (evt.clientY < i + 10 && evt.clientY > i - 10)
+				up = true;
+
+			else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
+				dw = true;
+
+			rg = true;
+
+			lf = true;
+			antX = evt.clientX;
+			antY = evt.clientY;
+		};
+
+		this.x6.onmousemove = evt => {
+			let i = parseInt(document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('top'));
+			let j = parseInt(document.defaultView.getComputedStyle(this.x1, null).getPropertyValue('height'));
+
+			if (evt.clientY < i + 10 && evt.clientY > i - 10)
+				this.x6.style.cursor = 'ne-resize';
+
+			else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
+				this.x6.style.cursor = 'se-resize';
+
+			else
+				this.x6.style.cursor = 'e-resize';
+		};
+
+		this.x6.onmousedown = evt => {
+			overEdge = true;
+			let i = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('top'));
+			let j = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('height'));
+
+			if (evt.clientY < i + 10 && evt.clientY > i - 10)
+				up = true;
+
+			else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
+				dw = true;
+
+			rg = true;
+			antX = evt.clientX;
+			antY = evt.clientY;
+		};
+
+		this.x2.onmousedown = function(evt) {
+			isBarPress = true;
+			antX = evt.clientX;
+			antY = evt.clientY;
+		};
+
+		onmouseup = function(evt) {
+			isBarPress = false;
+			overEdge = false;
+			up = false;
+			dw = false;
+			lf = false;
+			rg = false;
+		};
+		onmousemove = function(evt) {
+			if (isBarPress) {
+				let actX = evt.clientX;
+				let actY = evt.clientY;
+				let difX = actX - antX;
+				let difY = actY - antY;
+				antX = actX;
+				antY = actY;
+
+				let newDiv = x1;
+				let position = getPosition(newDiv);
+
+				newDiv.style.top = (position[0] + difY) + 'px';
+				newDiv.style.left = (position[1] + difX) + 'px';
+
+				if (position[0] + difY < 0)
+					newDiv.style.top = 0 + 'px';
+
+				else
+					newDiv.style.top = (position[0] + difY) + 'px';
+
+				if (position[1] + difX < 0)
+					newDiv.style.left = 0 + 'px';
+
+				else
+					newDiv.style.left = (position[1] + difX) + 'px';
+			}
+
+			if (overEdge) {
+				let actX = evt.clientX;
+				let actY = evt.clientY;
+				let difX = actX - antX;
+				let difY = actY - antY;
+				antX = actX;
+				antY = actY;
+
+				let newDiv = x1;
+
+				let dimension = getDimensions(newDiv);
+				let position = getPosition(newDiv);
+
+				console.log(difX);
+				console.log(dimension[0] + difX);
+				console.log(dimension[0]);
+
+				if (dimension[0] < 420 || dimension[1] < 340) {
+					if (dimension[1] < 340) {
+						newDiv.style.width = 340 + 'px';
+						return;
+					}
+
+					else {
+						newDiv.style.height = 420 + 'px';
+						return;
+					}
+				}
+
+				if (up) {
+					if (lf || rg) {
+						if(lf) {
+							newDiv.style.width = dimension[1] - difX + 'px';
+							newDiv.style.left = position[1] + difX + 'px';
+						}
+
+						else
+							newDiv.style.width = dimension[1] + difX + 'px';
+					}
+
+					newDiv.style.height = dimension[0] - difY + 'px';
+					newDiv.style.top = position[0] + difY + 'px';
+					return;
+				}
+
+				if (dw) {
+					if (lf || rg) {
+						if(lf) {
+							newDiv.style.width = dimension[1] - difX + 'px';
+							newDiv.style.left = position[1] + difX + 'px';
+						}
+
+						else
+							newDiv.style.width = dimension[1] + difX + 'px';
+					}
+
+					newDiv.style.height = dimension[0] + difY + 'px';
+					return;
+				}
+
+				if(lf) {
+					newDiv.style.width = dimension[1] - difX + 'px';
+					newDiv.style.left = position[1] + difX + 'px';
+					return;
+				}
+
+				if (rg) {
+					newDiv.style.width = dimension[1] + difX + 'px';
+					return;
+				}
+			}
+		};
+
+		function getPosition(element) {
+			let position = new Array(2);
+			position[0] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('top'));
+			position[1] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('left'));
+			return position;
+		}
+
+		function getDimensions(element) {
+			let dim = new Array(2);
+			dim[0] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('height'));
+			dim[1] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('width'));
+			return dim;
+		}
+
+	}
+
+	connectedCallback() {
+		let container = this.shadow.getElementById('container');
 
 		container.appendChild(this.addObj({type: 'button', id: 'btn1', top: '43%', left: '35.3%',
 			width: '95px', height: '25px', backgroundColor: 'rgba(0, 0, 0, 0.5)', border: 'none',
@@ -153,43 +440,13 @@ class pageEditor extends HTMLElement {
 		container.appendChild(this.addObj({type: 'table', id: 'mt', top: '52%', left: '2.4%', width: '95.5%',
 			height: '46%', backgroundColor: 'rgba(0, 0, 0, 0.3)', borderRadius: '2px', border: 'none'}));
 
-		this.x1 = this.shadow.getElementById('container');
-		this.x2 = this.shadow.getElementById('top-bar');
-		this.x3 = this.shadow.getElementById('top');
-		this.x4 = this.shadow.getElementById('down');
-		this.x5 = this.shadow.getElementById('left');
-		this.x6 = this.shadow.getElementById('right');
-		this.x7 = this.shadow.getElementById('redbtn');
-		this.x8 = this.shadow.getElementById('maxbtn');
-		this.x9 = this.shadow.getElementById('minbtn');
-
-		addEventListener('onmousemove', this.move);
-		addEventListener('onmouseup', this.up);
-		this.x2.addEventListener('onmousedown', this.x2down);
-		this.x3.addEventListener('onmousemove', this.x3move);
-		this.x3.addEventListener('onmousedown', this.x3down);
-		this.x4.addEventListener('onmousemove', this.x4move);
-		this.x4.addEventListener('onmousedown', this.x4down);
-		this.x5.addEventListener('onmousemove', this.x5move);
-		this.x5.addEventListener('onmousedown', this.x5down);
-		this.x6.addEventListener('onmousemove', this.x6move);
-		this.x6.addEventListener('onmousedown', this.x6down);
-		this.x7.addEventListener('onmouseover', this.x7over);
-		this.x7.addEventListener('onmouseleave', this.x7leave);
-		this.x7.addEventListener('onclick', this.x7click);
-		this.x8.addEventListener('onmouseover', this.x8over);
-		this.x8.addEventListener('onmouseleave', this.x8leave);
-		this.x8.addEventListener('onclick', this.x8click);
-		this.x9.addEventListener('onmouseover', this.x9over);
-		this.x9.addEventListener('onmouseleave', this.x9leave);
-		this.x9.addEventListener('onclick', this.x9click);
 	}
 
 	addObj(prop) {  //basicProperties (Array): typeElement, id, top, left, width, height
 									   //other (Obj): backColor, border, textAlign, fontSize, fontFamily
 		let element = document.createElement(prop.type);
 		element.id = prop.id;
-		element.textContent = prop.textContent
+		element.textContent = prop.textContent;
 		element.style.position = 'absolute';
 		element.style.top = prop.top;
 		element.style.left = prop.left;
@@ -205,295 +462,7 @@ class pageEditor extends HTMLElement {
 		return element;
 	}
 
-	x7over(evt) {
-		this.x7.style.backgroundColor = 'rgb(255, 0, 0)';
-	};
-
-	x7leave(evt) {
-		this.x7.style.backgroundColor = 'rgb(190, 0, 0)';
-	};
-
-	x7click(evt) {
-		this.x1.remove();
-	};
-
-	x8over(evt) {
-		this.x8.style.backgroundColor = 'rgb(70, 70, 100)';
-	};
-
-	x8leave(evt) {
-		this.x8.style.backgroundColor = 'rgb(100, 100, 140)';
-	};
-
-	x8click(evt) {
-		if(!isMax) {
-			top = x1.style.top;
-			left = x1.style.left;
-			width = x1.style.width;
-			height = x1.style.height;
-			this.x1.style.top = 0 + 'px';
-			this.x1.style.left = 0 + 'px';
-			this.x1.style.width = 100 + '%';
-			this.x1.style.height = 100 + '%';
-			isMax = true;
-		}
-
-		else {
-			this.x1.style.top = top;
-			this.x1.style.left = left;
-			this.x1.style.width = width;
-			this.x1.style.height = height;
-			isMax = false;
-		}
-	};
-
-	x9click(evt) {
-		if (!isHidden){
-			for (let elements of x1.children) {
-				if (elements.id === 'top-bar')
-					continue;
-				elements.style.visibility = 'hidden';
-			}
-			lastHeight = document.defaultView.getComputedStyle(x1, null).getPropertyValue('height');
-			this.x1.style.height = 28 + 'px';
-			isHidden = true;
-		}
-
-		else{
-			for (let elements of x1.children) {
-				if (elements.id === 'top-bar')
-					continue;
-				elements.style.visibility = 'visible';
-			}
-
-			this.x1.style.height = lastHeight;
-			isHidden = false;
-		}
-
-	};
-
-	x9over(evt) {
-		this.x9.style.backgroundColor = 'rgb(150, 150, 150)';
-	};
-
-	x9leave(evt) {
-		this.x9.style.backgroundColor = 'rgb(200, 200, 200)';
-	};
-
-	x3move(evt) {
-		this.x3.style.cursor = 'n-resize';
-	};
-
-	x3down(evt) {
-		overEdge = true;
-		up = true;
-		antX = evt.clientX;
-		antY = evt.clientY;
-	};
-
-	x4move(evt) {
-		this.x4.style.cursor = 's-resize';
-	};
-
-	x4down(evt) {
-		overEdge = true;
-		dw = true;
-		antX = evt.clientX;
-		antY = evt.clientY;
-	};
-
-	x5move(evt) {
-		let i = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('top'));
-		let j = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('height'));
-
-		if (evt.clientY < i + 10 && evt.clientY > i - 10)
-			this.x5.style.cursor = 'nw-resize';
-
-		else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
-			this.x5.style.cursor = 'sw-resize';
-
-		else
-			this.x5.style.cursor = 'w-resize';
-	};
-
-	x5down(evt) {
-		overEdge = true;
-
-		let i = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('top'));
-		let j = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('height'));
-
-		if (evt.clientY < i + 10 && evt.clientY > i - 10)
-			up = true;
-
-		else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
-			dw = true;
-
-		rg = true;
-
-		lf = true;
-		antX = evt.clientX;
-		antY = evt.clientY;
-	};
-
-	x6move(evt) {
-		let i = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('top'));
-		let j = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('height'));
-
-		if (evt.clientY < i + 10 && evt.clientY > i - 10)
-			this.x6.style.cursor = 'ne-resize';
-
-		else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
-			this.x6.style.cursor = 'se-resize';
-
-		else
-			this.x6.style.cursor = 'e-resize';
-	};
-
-	x6down(evt) {
-		overEdge = true;
-		let i = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('top'));
-		let j = parseInt(document.defaultView.getComputedStyle(x1, null).getPropertyValue('height'));
-
-		if (evt.clientY < i + 10 && evt.clientY > i - 10)
-			up = true;
-
-		else if (evt.clientY > i + j - 20 && evt.clientY < i + j + 20)
-			dw = true;
-
-		rg = true;
-		antX = evt.clientX;
-		antY = evt.clientY;
-	};
-
-	x2down(evt) {
-		isBarPress = true;
-		antX = evt.clientX;
-		antY = evt.clientY;
-	};
-
-	up(evt) {
-		isBarPress = false;
-		overEdge = false;
-		up = false;
-		dw = false;
-		lf = false;
-		rg = false;
-	};
-
-	move(evt) {
-		if (isBarPress) {
-			let actX = evt.clientX;
-			let actY = evt.clientY;
-			let difX = actX - antX;
-			let difY = actY - antY;
-			antX = actX;
-			antY = actY;
-
-			let newDiv = this.x1;
-			let position = this.getPosition(newDiv);
-
-			newDiv.style.top = (position[0] + difY) + 'px';
-			newDiv.style.left = (position[1] + difX) + 'px';
-
-			if (position[0] + difY < 0)
-				newDiv.style.top = 0 + 'px';
-
-			else
-				newDiv.style.top = (position[0] + difY) + 'px';
-
-			if (position[1] + difX < 0)
-				newDiv.style.left = 0 + 'px';
-
-			else
-				newDiv.style.left = (position[1] + difX) + 'px';
-		}
-
-		if (overEdge) {
-			let actX = evt.clientX;
-			let actY = evt.clientY;
-			let difX = actX - antX;
-			let difY = actY - antY;
-			antX = actX;
-			antY = actY;
-
-			let newDiv = this.x1;
-
-			let dimension = this.getDimensions(newDiv);
-			let position = this.getPosition(newDiv);
-
-			if (dimension[0] < 420 || dimension[1] < 340) {
-				if (dimension[1] < 340) {
-					newDiv.style.width = 340 + 'px';
-					return;
-				}
-
-				else {
-					newDiv.style.height = 420 + 'px';
-					return;
-				}
-			}
-
-			if (up) {
-				if (lf || rg) {
-					if(lf) {
-						newDiv.style.width = dimension[1] - difX + 'px';
-						newDiv.style.left = position[1] + difX + 'px';
-					}
-
-					else
-						newDiv.style.width = dimension[1] + difX + 'px';
-				}
-
-				newDiv.style.height = dimension[0] - difY + 'px';
-				newDiv.style.top = position[0] + difY + 'px';
-				return;
-			}
-
-			if (dw) {
-				if (lf || rg) {
-					if(lf) {
-						newDiv.style.width = dimension[1] - difX + 'px';
-						newDiv.style.left = position[1] + difX + 'px';
-					}
-
-					else
-						newDiv.style.width = dimension[1] + difX + 'px';
-				}
-
-				newDiv.style.height = dimension[0] + difY + 'px';
-				return;
-			}
-
-			if(lf) {
-				newDiv.style.width = dimension[1] - difX + 'px';
-				newDiv.style.left = position[1] + difX + 'px';
-				return;
-			}
-
-			if (rg) {
-				newDiv.style.width = dimension[1] + difX + 'px';
-				return;
-			}
-		}
-	};
-
-	getPosition(element) {
-		let position = new Array(2);
-		position[0] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('top'));
-		position[1] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('left'));
-		return position;
-	}
-
-	getDimensions(element) {
-		let dim = new Array(2);
-		dim[0] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('height'));
-		dim[1] = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue('width'));
-		return dim;
-	}
-
 }
 
 
 window.customElements.define('pg-editor', pageEditor);
-
-let a = new pageEditor();
